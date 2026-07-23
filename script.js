ad72d166e1ed6ed622e5bf66fbcf40a1e5e9a0a2
Novelea2026 <novelea2026@gmail.com>
2026-07-23 12:22:02 +0200

    Fetch prices from Supabase and update HTML
    Replaced hardcoded prices with dynamic loading from Supabase.

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
-document.getElementById("prijzen").innerHTML = `
-    <p>Knippen - €35</p>
-    <p>Scheren - €20</p>
-    <p>Massage - €50</p>
-`;
+const supabaseUrl = "https://ccmhegxkxyqemqbnqvro.supabase.co";
+const supabaseKey = "HIER_JE_PUBLISHABLE_KEY";
+
+const client = supabase.createClient(supabaseUrl, supabaseKey);
+
+async function laadPrijzen() {
+    const { data, error } = await client
+        .from("prijzen")
+        .select("*");
+
+    if (error) {
+        console.error(error);
+        return;
+    }
+
+    let html = "";
+
+    data.forEach(item => {
+        html += `<p>${item.naam} - €${item.prijs}</p>`;
+    });
+
+    document.getElementById("prijzen").innerHTML = html;
+}
+
+laadPrijzen();
